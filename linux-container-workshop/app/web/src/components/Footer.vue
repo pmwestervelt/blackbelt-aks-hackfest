cad75148b7310d710e711f6b754c511708e1f46c
Paul Westervelt <paulw@microsoft.com>
2018-12-05 18:56:25 -0600

    Spiralling furth into HELL!

--- linux-container-workshop/app/web/src/components/Footer.vue
+++ linux-container-workshop/app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team Rocks
+        PMW Changing the display to what ever!
       </div>
       <div class="col-lg-6">
       </div>
